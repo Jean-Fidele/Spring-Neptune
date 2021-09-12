cf36eddf4b07092cdd462a7730d48d5f51accec0
Fidele <fidele999@gmail.com>
2021-09-12 04:31:54 +0200

    Teste submit formulaire

--- src/main/java/neptune/admin/controllers/LoginController.java
+++ src/main/java/neptune/admin/controllers/LoginController.java
@@ -3,8 +3,10 @@ package neptune.admin.controllers;
 import neptune.admin.metiers.IUserMetier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -14,8 +16,17 @@ public class LoginController {
     @Autowired
     private IUserMetier userMetier;
 
-    @RequestMapping(path = "/login")
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
     public String index() {
+        return "admin/login";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String index(Model model, String email, String password) {
+        if(email != ""){
+            model.addAttribute("message", email);
+            return "admin/index";
+        }
         return "admin/login";
     }
 
